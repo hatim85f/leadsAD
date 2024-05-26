5ae11275730e9fbd18156fd682abf86033493d9d
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-05-27 01:38:18 +0400

    editing user

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find().populate("-password");
+    const users = await User.find().populate("password");
 
     return res.status(200).json({
       users,
